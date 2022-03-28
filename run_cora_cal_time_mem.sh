293ba1c66674553a5db377fa8e65a74082e366ee
Chameleon Cloud User <cc@ysy-rtx-6000-1.novalocal>
2022-03-28 20:51:48 +0000

    cora range random

---
+++ run_cora_cal_time_mem.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+model=sage
+file=cora
+# aggre=lstm
+# pMethod=range
+
+hiddenList=(16 32 64 128 256)
+layersList=(3 4 5 6)
+aggreList=(mean lstm)
+pMethodList=(random range)
+eval=False
+
+for aggre in ${aggreList[@]}
+do      
+        
+        mkdir ${file}/${model}/res/${aggre}/
+        for pMethod in ${pMethodList[@]}
+        do
+                mkdir ${file}/${model}/res/${aggre}/${pMethod}
+                for layers in ${layersList[@]}
+                do
+                        mkdir ${file}/${model}/res/${aggre}/${pMethod}/layers_${layers}       
+                        for hidden in ${hiddenList[@]}
+                        do      
+                                mkdir ${file}/${model}/res/${aggre}/${pMethod}/layers_${layers}/h_${hidden} 
+                                resPath=${file}/${model}/res/${aggre}/${pMethod}/layers_${layers}/h_${hidden}
+                                
+                                python calculate_time_mem.py \
+                                --aggre $aggre \
+                                --selection-method $pMethod \
+                                --hidden $hidden \
+                                --num-layers $layers \
+                                --save-path ${resPath}/${file}_${model}_eval_${eval}_pseudo_ \
+                                > ${resPath}/${file}_${model}_eval_${eval}_mem.log
+                    
+                                python calculate_compute_efficiency.py \
+                                --aggre $aggre \
+                                --selection-method $pMethod \
+                                --hidden $hidden \
+                                --num-layers $layers \
+                                --save-path ${resPath}/${file}_${model}_eval_${eval}_pseudo_ \
+                                > ${resPath}/${file}_${model}_eval_${eval}_eff.log
+                        done
+                done
+        done
+done
+# model=sage
+# file=arxiv
+# aggre=mean
+# eval=False
+
+# python calculate_time_mem.py \
+# --save-path ${model}/res/${aggre}/${file}_${model}_eval_${eval}_pseudo_ \
+# > ${model}/res/${aggre}/${file}_${model}_eval_${eval}_mem.log
+
+# python calculate_compute_efficiency.py \
+# --save-path ${model}/res/${aggre}/${file}_${model}_eval_${eval}_pseudo_ \
+# > ${model}/res/${aggre}/${file}_${model}_eval_${eval}_eff.log
